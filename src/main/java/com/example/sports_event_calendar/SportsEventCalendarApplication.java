3e040cec63a1ae6bd88c7f3fec5a574dc66fe643
Taku-pg <kaitsu505@gmail.com>
2026-03-24 22:39:07 +0100

    Initial commit

---
+++ src/main/java/com/example/sports_event_calendar/SportsEventCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.sports_event_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEventCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsEventCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sports_event_calendar/SportsEventCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sports_event_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsEventCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
