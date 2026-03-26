b8a050493f5b1e52f5534dc7fbdfa7eaa20669ff
Taku-pg <kaitsu505@gmail.com>
2026-03-26 11:24:13 +0100

    create: create venue repository

---
+++ src/main/java/com/example/sports_event_calendar/Repositories/VenueRepository.java
@@ -0,0 +1,26 @@
+package com.example.sports_event_calendar.Repositories;
+
+import com.example.sports_event_calendar.Models.DTOs.AddressDTO;
+import com.example.sports_event_calendar.Models.Entities.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface VenueRepository extends JpaRepository<Venue,Long> {
+
+    @Query("""
+    SELECT new com.example.sports_event_calendar.Models.DTOs.AddressDTO(
+         v.venueName, 
+         v.buildingNumber, 
+         s.streetName,
+         ci.cityName, 
+         co.countryName
+             )
+        FROM Venue v 
+            JOIN v.street s
+            JOIN s.city ci
+            JOIN ci.country co 
+                WHERE v.id = :venueId
+    """)
+    public AddressDTO getAddress(@Param("venueId")  Long venueId);
+}
