f0c338439810e12ee8a94881256a740e2cc655f2
Taku-pg <kaitsu505@gmail.com>
2026-03-26 21:46:47 +0100

    add: add method to query team by sport type

--- src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
+++ src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
@@ -2,9 +2,14 @@ package com.example.sports_event_calendar.Repositories;
 
 import com.example.sports_event_calendar.Models.Entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Integer> {
     Optional<Team> findTeamByTeamName(String teamName);
+
+    @Query("SELECT t.teamName FROM Team t WHERE t.sportType.sportType = :sportType")
+    List<String> findTeamNameBySportType(String sportType);
 }
