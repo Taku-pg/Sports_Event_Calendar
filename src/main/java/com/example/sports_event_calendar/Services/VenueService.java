1527e49a373c310cdd1ea486293638748c734c3c
Taku-pg <kaitsu505@gmail.com>
2026-03-26 21:48:09 +0100

    create: create venue service class

---
+++ src/main/java/com/example/sports_event_calendar/Services/VenueService.java
@@ -0,0 +1,19 @@
+package com.example.sports_event_calendar.Services;
+
+import com.example.sports_event_calendar.Models.Entities.Venue;
+import com.example.sports_event_calendar.Repositories.VenueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VenueService {
+    public final VenueRepository venueRepository;
+    public VenueService(VenueRepository venueRepository) {
+        this.venueRepository = venueRepository;
+    }
+
+    public List<Venue> getAllVenues() {
+        return venueRepository.findAll();
+    }
+}
