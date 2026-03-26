be3dc44cde767664b65f5833ba78a81e35b3a9e4
Taku-pg <kaitsu505@gmail.com>
2026-03-26 11:25:51 +0100

    add: add end-point for event detail view

--- src/main/java/com/example/sports_event_calendar/Controllers/EventController.java
+++ src/main/java/com/example/sports_event_calendar/Controllers/EventController.java
@@ -1,6 +1,7 @@
 package com.example.sports_event_calendar.Controllers;
 
 import com.example.sports_event_calendar.Models.DTOs.EventDTO;
+import com.example.sports_event_calendar.Models.DTOs.EventDetailDTO;
 import com.example.sports_event_calendar.Models.Entities.Event;
 import com.example.sports_event_calendar.Services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,10 @@ public class EventController {
         return "events";
     }
 
-    @GetMapping("/events/{eventId}")
+    @GetMapping("/events")
     public String eventDetails(Model model, @RequestParam(name = "eventId") Long eventId) {
-
-        return "eventDetails";
+        EventDetailDTO eventDetailDTO = eventService.findEventDetailById(eventId);
+        model.addAttribute("eventDetailDTO", eventDetailDTO);
+        return "event_detail";
     }
 }
