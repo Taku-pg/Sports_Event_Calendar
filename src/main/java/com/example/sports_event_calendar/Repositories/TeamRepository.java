38615f37bb50a57c4099e82083eee8922f35a9c3
Taku-pg <kaitsu505@gmail.com>
2026-03-27 17:21:08 +0100

    fix: fix get team by sport type method

--- src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
+++ src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
@@ -1,8 +1,8 @@
 package com.example.sports_event_calendar.Repositories;
 
+import com.example.sports_event_calendar.Models.Entities.SportType;
 import com.example.sports_event_calendar.Models.Entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +10,5 @@ import java.util.Optional;
 public interface TeamRepository extends JpaRepository<Team, Integer> {
     Optional<Team> findTeamByTeamName(String teamName);
 
-    @Query("SELECT t.teamName FROM Team t WHERE t.sportType.sportType = :sportType")
-    List<String> findTeamNameBySportType(String sportType);
+    List<Team> findTeamBySportType(SportType sportType);
 }
