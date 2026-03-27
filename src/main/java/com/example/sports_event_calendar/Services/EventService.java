c485840d71b68112ea809207c9f93bd432645c73
Taku-pg <kaitsu505@gmail.com>
2026-03-27 17:57:22 +0100

    add: add register new event method

--- src/main/java/com/example/sports_event_calendar/Services/EventService.java
+++ src/main/java/com/example/sports_event_calendar/Services/EventService.java
@@ -3,17 +3,17 @@ package com.example.sports_event_calendar.Services;
 import com.example.sports_event_calendar.Models.DTOs.AddressDTO;
 import com.example.sports_event_calendar.Models.DTOs.EventDTO;
 import com.example.sports_event_calendar.Models.DTOs.EventDetailDTO;
+import com.example.sports_event_calendar.Models.DTOs.NewEventDTO;
 import com.example.sports_event_calendar.Models.Entities.Event;
 import com.example.sports_event_calendar.Repositories.EventRepository;
 import com.example.sports_event_calendar.Repositories.VenueRepository;
 import com.example.sports_event_calendar.Services.Mappers.EventDetailMapper;
 import com.example.sports_event_calendar.Services.Mappers.EventMapper;
+import com.example.sports_event_calendar.Services.Mappers.NewEventMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
 
 
 @Service
@@ -22,15 +22,18 @@ public class EventService {
     private final VenueRepository venueRepository;
     private final EventMapper eventMapper;
     private final EventDetailMapper eventDetailMapper;
+    private final NewEventMapper newEventMapper;
 
     public EventService(EventRepository eventRepository,
                         VenueRepository venueRepository,
                         EventMapper eventMapper,
-                        EventDetailMapper eventDetailMapper) {
+                        EventDetailMapper eventDetailMapper,
+                        NewEventMapper newEventMapper) {
         this.eventRepository = eventRepository;
         this.venueRepository = venueRepository;
         this.eventMapper = eventMapper;
         this.eventDetailMapper = eventDetailMapper;
+        this.newEventMapper = newEventMapper;
     }
 
     public List<EventDTO> findAll() {
@@ -41,6 +44,11 @@ public class EventService {
         Event event = eventRepository.findById(id).orElseThrow();
         AddressDTO addressDTO = venueRepository.getAddress(event.getVenue().getId());
         return eventDetailMapper.mapToEventDetail(event,addressDTO);
+    }
+
+    public void addNewEvent(NewEventDTO newEventDTO) throws NoSuchElementException {
+        Event newEvent = newEventMapper.mapToEvent(newEventDTO);
+        eventRepository.save(newEvent);
     }
 
 }
