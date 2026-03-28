a5d77adf6fc79e69095fecee2f7f51756cbc60d5
Taku-pg <kaitsu505@gmail.com>
2026-03-28 15:31:38 +0100

    delete: delete query

--- src/main/java/com/example/sports_event_calendar/Repositories/EventRepository.java
+++ src/main/java/com/example/sports_event_calendar/Repositories/EventRepository.java
@@ -1,27 +1,8 @@
 package com.example.sports_event_calendar.Repositories;
 
 
-import com.example.sports_event_calendar.Models.DTOs.EventDTO;
 import com.example.sports_event_calendar.Models.Entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-    @Query("""
-            SELECT new com.example.sports_event_calendar.Models.DTOs.EventDTO(
-                        e.id, 
-                        e.eventName, 
-                        t1.teamName, 
-                        t2.teamName, 
-                        st.sportType
-            ) 
-            FROM Event e
-            JOIN e.firstTeam t1
-            JOIN e.secondTeam t2
-            JOIN t1.sportType st 
-            ORDER BY e.eventDate 
-            """)
-    public List<EventDTO> findAllEvents();
 }
