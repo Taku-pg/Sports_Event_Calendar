5aa2b044df86b6b3c3cb885b0fa91de14ecd3a53
Taku-pg <kaitsu505@gmail.com>
2026-03-29 17:18:03 +0200

    fix: modify repository for custom query

--- src/main/java/com/example/sports_event_calendar/Repositories/SportTypeRepository.java
+++ src/main/java/com/example/sports_event_calendar/Repositories/SportTypeRepository.java
@@ -1,12 +1,19 @@
 package com.example.sports_event_calendar.Repositories;
 
+import com.example.sports_event_calendar.Models.DTOs.SportTypeDTO;
 import com.example.sports_event_calendar.Models.Entities.SportType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface SportTypeRepository extends JpaRepository<SportType, Long> {
-    Optional<SportType> findSportTypeBySportType(String sportType);
 
-    Optional<SportType> findSportTypeById(Long id);
+    @Query("SELECT new com.example.sports_event_calendar.Models.DTOs.SportTypeDTO( st.id, st.sportType) FROM SportType st")
+    List<SportTypeDTO> findAllDTO();
+
+    @Query("SELECT st.sportType FROM SportType st WHERE st.id = :id")
+    String findSportTypeName(@Param("id") Long id);
+
 }
