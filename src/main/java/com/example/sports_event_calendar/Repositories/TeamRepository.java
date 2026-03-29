21a4279bd26f27c3a919343c6f20bdae62e12007
Taku-pg <kaitsu505@gmail.com>
2026-03-29 17:18:15 +0200

    fix: modify repository for custom query

--- src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
+++ src/main/java/com/example/sports_event_calendar/Repositories/TeamRepository.java
@@ -1,15 +1,33 @@
 package com.example.sports_event_calendar.Repositories;
 
+import com.example.sports_event_calendar.Models.DTOs.TeamDTO;
 import com.example.sports_event_calendar.Models.Entities.SportType;
 import com.example.sports_event_calendar.Models.Entities.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-    Optional<Team> findTeamByTeamName(String teamName);
+public interface TeamRepository extends JpaRepository<Team, Long> {
 
-    List<Team> findTeamBySportType(SportType sportType);
     Optional<Team> findTeamById(Long id);
+
+    @Query("""
+           SELECT new com.example.sports_event_calendar.Models.DTOs.TeamDTO(
+                      t.id,
+                      t.teamName
+                      )FROM Team t WHERE t.sportType.sportType = :sportTypeName
+           """)
+    List<TeamDTO> findTeamDTOSBySportTypeName(@Param("sportTypeName") String sportTypeName);
+
+    @Query("""
+           SELECT new com.example.sports_event_calendar.Models.DTOs.TeamDTO(
+                      t.id,
+                      t.teamName,
+                      st.sportType
+                      )FROM Team t JOIN t.sportType st WHERE t.id = :id
+           """)
+    Optional<TeamDTO> findTeamDTOById(@Param("id") Long id);
 }
