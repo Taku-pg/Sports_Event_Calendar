256b919ef6413199660b48fedccd723af7643c24
Taku-pg <kaitsu505@gmail.com>
2026-03-29 17:30:04 +0200

    fix: modify method getTeamDTOS

--- src/main/java/com/example/sports_event_calendar/Services/TeamService.java
+++ src/main/java/com/example/sports_event_calendar/Services/TeamService.java
@@ -1,10 +1,8 @@
 package com.example.sports_event_calendar.Services;
 
 import com.example.sports_event_calendar.Models.DTOs.TeamDTO;
-import com.example.sports_event_calendar.Models.Entities.SportType;
 import com.example.sports_event_calendar.Models.Entities.Team;
 import com.example.sports_event_calendar.Repositories.TeamRepository;
-import com.example.sports_event_calendar.Services.Mappers.TeamMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,18 +10,13 @@ import java.util.List;
 @Service
 public class TeamService {
     private final TeamRepository teamRepository;
-    private final TeamMapper teamMapper;
 
-    public TeamService(TeamRepository teamRepository,  TeamMapper teamMapper) {
+    public TeamService(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
-        this.teamMapper = teamMapper;
     }
 
-    public List<TeamDTO> getTeamDTOS(SportType sportType) {
-        return teamRepository.findTeamBySportType(sportType)
-                .stream()
-                .map(teamMapper::toTeamDTO)
-                .toList();
+    public List<TeamDTO> getTeamDTOS(String sportTypeName) {
+        return teamRepository.findTeamDTOSBySportTypeName(sportTypeName);
     }
 
     public Team findTeamById(Long id){
